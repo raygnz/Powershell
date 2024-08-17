b590534d2b11ffff8341a2819ce0af2d52f8eccf
raygnz <71155786+raygnz@users.noreply.github.com>
2024-08-17 15:41:54 +1200

    Add test-connection

--- Windows Services/Start-WindowsServicesStopped.ps1
+++ Windows Services/Start-WindowsServicesStopped.ps1
@@ -5,29 +5,28 @@ Starts a stopped service on a remote computer(s).
 Connects to a remote computer. Checks for a stopped service and then starts it.
 .NOTES
 $ServerList = "<target computername>"
-$Cred = "<Admin account with permissions to run the script>"
 $ServiceName = "<Windows Service you wish to start"
 Author: Phil Gray
 Email: philiphgray@gmail.com
 URL: https://github.com/raygnz/Powershell
 Version 1.0 - 16/08/24 - Initial
+Version 1.1 - 17/08/24 - Add Test Connection to verify remote server
 #>
 
 # Variables
-$ServerList = "C:\Temp\ServerList.txt"
-$Creds = Read-Host "Enter Admin Username"
+$ServerList = Get-Content "C:\Temp\ServerList.txt"
 $ServiceName = Read-Host "Enter Windows Service Name"
 
-#Test connection to remote servers
-If (Test-Connection -TargetName $ServerList -Quiet)
-{
+# Test connection to remote servers
+foreach ( $Server in $ServerList ) {
+   if (Test-Connection -TargetName $Server -Count 1 -Quiet) {
+# Connect to remote server and start stopped service
+     try {
+        Invoke-Command -ComputerName $Server {Get-Service $using:ServiceName |
+        Where-Object {$_.status -eq 'stopped'} | Start-Service}
+     }
+    catch { Write-Host "Failed to connect to $Server"
+        }
+    }
+}
 
-# Start stopped service on remote computer if the service is stopped
-$Computername = (Get-Content -path $ServerList)
-foreach ( $computer in $Computername ) {Invoke-Command -ComputerName $computer -Credential $Creds {
-    Get-Service $using:ServiceName | Where-Object {$_.status -eq 'stopped'} | Start-Service }
- }
- else {
-    Write-Warning "Remote computer unreachable"    
-}
-}
